926d64faf3e1790e2446436cfc5d9dc8c96979c0
Little Bit <jeffreyblock@outlook.com>
2018-12-28 22:32:48 -0600

    Update Script

--- js/script.js
+++ js/script.js
@@ -10,12 +10,13 @@ document.addEventListener('DOMContentLoaded', () => {
 // GLOBAL VARIABLES ***************************************************
 // Capture list of student li elements in the const 'studentList'
 const studentList = document.getElementsByClassName('student-item');
-//???????????Capture list of all page li elements in the const 'page'
-//???????????const page = document.getElementsByClassName('pageLi'); // Do I need this?
+const firstPage = 1;
+//Capture list of all page li elements in the const 'page'
+//const page = document.getElementsByClassName('pageLi'); 
 
 
 
-// START OF 'showPage' FUNCTION *****************************************
+// START OF 'showPage' FUNCTION ***************************************
 // Create/define the function to show 10 'studentList' items per page
 const showPage = (list, page) => { 
    // Define the first 'studentList' item and the last 'studentList' item on the page
@@ -25,26 +26,23 @@ const showPage = (list, page) => {
    //Loop over items in the list parameter
    for (let i = 0; i < list.length; i++) {
       // Set conditions for when 'studentList' items appear on the page
-      if (list[i] >= firstListItem && list[i] <= lastListItem) {
+      if (i >= firstListItem && i <= lastListItem) {
          list[i].style.display = 'block';
       } else {
          list[i].style.display = 'none';
       }
-   }
-      // Return the 10 'studentList' items to be displayed for use in the appendPageLinks function
-      return list;
+   }      
 }
-// END OF 'showPage' FUNCTION *******************************************
+// END OF 'showPage' FUNCTION ****************************************
 
 
 
-// START OF 'appendPageLinks' FUNCTION **********************************
+// START OF 'appendPageLinks' FUNCTION *******************************
 // Create/define the function to generate, append, and add functionality to the pagination buttons.
 const appendPageLinks = (list) => {
    
    // Determine how many pages are needed for the student list 
    let pagesNeeded = Math.ceil(studentList.length / 10);
-   
    // Create a DIV and give it the class of 'pagination' class and append it to the .page DIV
    const paginationDiv = document.createElement('div');
    paginationDiv.className = 'pagination';
@@ -57,24 +55,44 @@ const appendPageLinks = (list) => {
 
    // Add a UL to the 'pagination' DIV to store the pagination links
    let pageLinksContainer = document.createElement('ul');
+   pageLinksContainer.className = 'pageLiContainer';
    paginationDiv.appendChild(pageLinksContainer);
 
    // FOR every page, add LI and A tags with the page number text
-   for (let i = 0; i < pagesNeeded.length; i++) {
-
+   const pageLi = document.createElement('li');
+   for (let i = 0; i < pagesNeeded; i++) {
+      pageLinksContainer.appendChild(pageLi);
+      let a = document.createElement('a');
+      a.setAttribute('href', '#');
+      a.textContent = i + 1;
+      pageLi.appendChild(a);
+      if (i === 0) {
+         a.className = 'active';
+      }
    }
 
    // Add an event listener to each A tag.  When they are clicked, call the showPage function to display the appropriate page
-   // Loop over pagination links to remove the active class from all links
-   // Add the active class to the link that was just clicked.  You can identify the clicked link using event.target
+   pageLinksContainer.addEventListener('click', (e) => {
+      let currentPage = e.target.textContent;
+      showPage(list, currentPage);
    
+   
+   // Loop over pagination links to remove the active class from all links
+   const pageButtons = document.querySelectorAll('a');
+   for (let i = 0; i < pageButtons.length; i++) {
+      pageButtons[i].classList.remove('active');
+   }
+
+   // Add the active class to the link that was just clicked.  You can identify the clicked link using event.target
+   e.target.className = 'active';
+});
 }
-
-// END OF 'appendPageLinks' FUNCTION ************************************
-
+// END OF 'appendPageLinks' FUNCTION **********************************
 
 
-
+// Call both functions to run
+showPage(studentList, firstPage);
+appendPageLinks(studentList);
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
 
